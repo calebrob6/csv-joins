4ee06f3ccace6ca379ba20956d8cbb10ca94a4e2
Caleb Robinson <dcrobins@gatech.edu>
2017-02-23 00:12:40 -0500

    0. Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,119 @@
 # csvJoins
-Python script for performing different join operations on CSV files.
+Small python script for performing different join operations on CSV files.
+
+This script can perform left, right, inner, and full outer joins on two arbitrary CSV files.
+It takes two filenames, and the column names of the columns to use as the primary key values.
+It requires all the values in each primary key column to be unique. 
+
+## Usage
+
+```
+usage: join.py [-h] [-v] [-t {left,right,inner,full}]
+               leftFn leftPK rightFn rightPK outputFn
+
+CSV join script
+
+positional arguments:
+  leftFn                Filename of left table
+  leftPK                Name of column to use as the primary key for the left
+                        table
+  rightFn               Filename of the right table
+  rightPK               Name of column to use as the primary key for the right
+                        table
+  outputFn              Output filename
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -v, --verbose         Show output from latex and dvipng commands
+  -t {left,right,inner,full}, --type {left,right,inner,full}
+                        Type of join (default is 'left join')
+```
+
+# Examples
+
+The following are examples that operate on the "a.csv" and "b.csv" files (shown below).
+
+The example images are taken from this stackoverflow post: [http://stackoverflow.com/questions/17946221/sql-join-and-different-types-of-joins](http://stackoverflow.com/questions/17946221/sql-join-and-different-types-of-joins).
+
+### a.csv
+
+```
+a,b,c,d,e
+97,38,15,7,23
+8,41,15,85,50
+83,94,10,84,21
+43,29,68,87,4
+85,54,37,7,24
+```
+
+### b.csv
+
+```
+l,m,n,o,p
+12,18,9,54,76
+24,92,61,42,9
+26,72,62,14,23
+53,61,49,92,26
+16,83,53,41,75
+
+```
+
+## Left Join
+
+![left join example](images/left.png)
+
+`python join.py a.csv "a" b.csv "m" output.csv -t left`
+
+```
+a,b,c,d,e,l,m,n,o,p
+97,38,15,7,23,null,null,null,null,null
+8,41,15,85,50,null,null,null,null,null
+83,94,10,84,21,16,83,53,41,75
+43,29,68,87,4,null,null,null,null,null
+85,54,37,7,24,null,null,null,null,null
+```
+
+## Right Join
+
+![right join example](images/right.png)
+
+`python join.py a.csv "a" b.csv "m" output.csv -t right`
+
+```
+a,b,c,d,e,l,m,n,o,p
+null,null,null,null,null,12,18,9,54,76
+null,null,null,null,null,24,92,61,42,9
+null,null,null,null,null,26,72,62,14,23
+null,null,null,null,null,53,61,49,92,26
+83,94,10,84,21,16,83,53,41,75
+```
+
+## Inner Join
+
+![inner join example](images/inner.png)
+
+`python join.py a.csv "a" b.csv "m" output.csv -t inner`
+
+```
+a,b,c,d,e,l,m,n,o,p
+83,94,10,84,21,16,83,53,41,75
+```
+
+## Full Outer Join
+
+![full join example](images/full.png)
+
+`python join.py a.csv "a" b.csv "m" output.csv -t full`
+
+```
+a,b,c,d,e,l,m,n,o,p
+null,null,null,null,null,12,18,9,54,76
+43,29,68,87,4,null,null,null,null,null
+null,null,null,null,null,53,61,49,92,26
+null,null,null,null,null,26,72,62,14,23
+8,41,15,85,50,null,null,null,null,null
+83,94,10,84,21,16,83,53,41,75
+null,null,null,null,null,24,92,61,42,9
+97,38,15,7,23,null,null,null,null,null
+85,54,37,7,24,null,null,null,null,null
+```
---
+++ join.py
@@ -0,0 +1,179 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2017 Caleb Robinson <calebrob6@gmail.com>
+#
+# Distributed under terms of the MIT license.
+
+import sys
+import time
+import csv
+import argparse
+
+def loadCSV(fn,header=False,DELIM="|",QUOTECHAR='"'):
+    
+    f = open(fn,"rb")
+    csvReader = csv.reader(f, delimiter=DELIM, quotechar=QUOTECHAR)
+    
+    if header:
+        headerLine = csvReader.next()
+
+    data = []
+    for row in csvReader:
+        data.append(row)
+
+    f.close()
+
+    if header:
+        return headerLine, data
+    else:
+        return data
+    
+
+def metaLoadCSVFile(fn, pk):
+    header, data = loadCSV(fn, header=True, DELIM=",")
+
+    if pk not in header:
+        raise ValueError("Error: primary key (%s) not in header line of %s" % (pk, fn))
+    
+    # Find the index of the primary key column
+    pkIndex = header.index(pk)
+
+    # Make sure that our primary key column has all unique values
+    pkSet = set()
+    for row in data:
+        if row[pkIndex] in pkSet:
+            raise ValueError("Error: primary key column is not unique in %s (duplicate value found: %s)" % (fn, row[pkIndex]))
+        else:
+            pkSet.add(row[pkIndex])
+
+    return header, data, pkIndex
+
+def doArgs(argList, name):
+    parser = argparse.ArgumentParser(description=name)
+
+    parser.add_argument("-v", "--verbose", action="store_true", help="Show output from latex and dvipng commands", default=False)
+    parser.add_argument("leftFn", type=str, help="Filename of left table")
+    parser.add_argument("leftPK", type=str, help="Name of column to use as the primary key for the left table")
+    parser.add_argument("rightFn", type=str, help="Filename of the right table")
+    parser.add_argument("rightPK", type=str, help="Name of column to use as the primary key for the right table")
+    parser.add_argument("outputFn", type=str, help="Output filename")
+    parser.add_argument("-t", "--type",  type=str, action="store", dest="joinType", choices=["left","right","inner","full"], help="Type of join (default is 'left join')", default="left")
+
+    return parser.parse_args(argList)
+
+def main():
+
+    args = doArgs(sys.argv[1:], "CSV join script")
+    startTime = float(time.time())
+
+    #-------------------------------------------------------------------------------------------
+    # Load the left data file
+    #-------------------------------------------------------------------------------------------
+    leftFn = args.leftFn
+    leftPK = args.leftPK
+
+    leftHeader, leftData, leftKeyIndex = metaLoadCSVFile(leftFn, leftPK)
+
+    # Map the primary keys to their row index so we can look up keys from the other table in constant time
+    leftPKMap = {}
+    for i,row in enumerate(leftData):
+        leftPKMap[row[leftKeyIndex]] = i
+
+    #-------------------------------------------------------------------------------------------
+    # Load the right data file
+    #-------------------------------------------------------------------------------------------
+    rightFn = args.rightFn
+    rightPK = args.rightPK
+
+    rightHeader, rightData, rightKeyIndex = metaLoadCSVFile(rightFn, rightPK)
+
+    # Map the primary keys to their row index so we can look up keys from the other table in constant time
+    rightPKMap = {}
+    for i,row in enumerate(rightData):
+        rightPKMap[row[rightKeyIndex]] = i
+
+    #-------------------------------------------------------------------------------------------
+    # Write output file
+    #-------------------------------------------------------------------------------------------
+    outputFn = args.outputFn
+    joinType = args.joinType
+
+    f = open(outputFn, 'wb')
+    csvWriter = csv.writer(f, delimiter=',',quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    csvWriter.writerow(leftHeader + rightHeader)
+
+    #-------------------------------------------------------------------------------------------
+    if joinType == "left":
+
+        # Iterate through each row in the left table, try to find the matching row in the right table
+        # if the matching row doesn't exist, then fill with 'null's, if it does, then copy it over
+        for leftRow in leftData:
+            leftRowPK = leftRow[leftKeyIndex]
+            rightRow = None
+            if leftRowPK in rightPKMap:
+                rightRow = rightData[rightPKMap[leftRowPK]]
+            else:
+                rightRow = ["null"] * len(rightHeader)
+
+            csvWriter.writerow(leftRow + rightRow)
+    #-------------------------------------------------------------------------------------------
+    elif joinType == "right":
+
+        # Similar to 'left' case
+        for rightRow in rightData:
+            rightRowPK = rightRow[rightKeyIndex]
+            leftRow = None
+            if rightRowPK in leftPKMap:
+                leftRow = leftData[leftPKMap[rightRowPK]]
+            else:
+                leftRow = ["null"] * len(leftHeader)
+
+            csvWriter.writerow(leftRow + rightRow)
+    #-------------------------------------------------------------------------------------------
+    elif joinType == "inner":
+        # This join will only write rows for primary keys in the intersection of the two primary key sets
+        leftKeySet = set(leftPKMap.keys())
+        rightKeySet = set(rightPKMap.keys())
+
+        newKeys = leftKeySet & rightKeySet
+
+        for keyVal in newKeys:
+            leftRow = leftData[leftPKMap[keyVal]]
+            rightRow = rightData[rightPKMap[keyVal]]
+
+            csvWriter.writerow(leftRow + rightRow)
+    #-------------------------------------------------------------------------------------------
+    elif joinType == "full":
+        # This join will only write rows for primary keys in the union of the two primary key sets
+        leftKeySet = set(leftPKMap.keys())
+        rightKeySet = set(rightPKMap.keys())
+
+        newKeys = leftKeySet | rightKeySet
+
+        for keyVal in newKeys:
+            leftRow = None
+            if keyVal in leftPKMap:
+                leftRow = leftData[leftPKMap[keyVal]]
+            else:
+                leftRow = ["null"] * len(leftHeader)
+            
+            rightRow = None
+            if keyVal in rightPKMap:
+                rightRow = rightData[rightPKMap[keyVal]]
+            else:
+                rightRow = ["null"] * len(rightHeader)
+
+            csvWriter.writerow(leftRow + rightRow)
+    #-------------------------------------------------------------------------------------------
+    else:
+        raise Exception("This shouldn't happen")
+
+    f.close()
+
+    print "Finished in %0.4f seconds" % (time.time() - startTime)
+
+
+if __name__ == "__main__":
+    main()
