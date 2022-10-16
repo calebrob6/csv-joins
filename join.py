711acc10554c582afdb17c92464240d5f3b60113
Ronald Theodoro <ronaldtheodoro@gmail.com>
2022-10-16 16:41:15 -0300

    Format code with black

--- join.py
+++ join.py
@@ -11,11 +11,12 @@ import time
 import csv
 import argparse
 
-def loadCSV(fn,header=False,DELIM="|",QUOTECHAR='"'):
-    
-    f = open(fn,"r")
+
+def loadCSV(fn, header=False, DELIM="|", QUOTECHAR='"'):
+
+    f = open(fn, "r")
     csvReader = csv.reader(f, delimiter=DELIM, quotechar=QUOTECHAR)
-    
+
     if header:
         headerLine = next(csvReader)
 
@@ -29,14 +30,16 @@ def loadCSV(fn,header=False,DELIM="|",QUOTECHAR='"'):
         return headerLine, data
     else:
         return data
-    
+
 
 def metaLoadCSVFile(fn, pk):
     header, data = loadCSV(fn, header=True, DELIM=",")
 
     if pk not in header:
-        raise ValueError("Error: primary key (%s) not in header line of %s" % (pk, fn))
-    
+        raise ValueError(
+            "Error: primary key (%s) not in header line of %s" % (pk, fn)
+        )
+
     # Find the index of the primary key column
     pkIndex = header.index(pk)
 
@@ -44,33 +47,63 @@ def metaLoadCSVFile(fn, pk):
     pkSet = set()
     for row in data:
         if row[pkIndex] in pkSet:
-            raise ValueError("Error: primary key column is not unique in %s (duplicate value found: %s)" % (fn, row[pkIndex]))
+            raise ValueError(
+                "Error: primary key column is not unique in %s (duplicate value found: %s)"
+                % (fn, row[pkIndex])
+            )
         else:
             pkSet.add(row[pkIndex])
 
     return header, data, pkIndex
 
+
 def doArgs(argList, name):
     parser = argparse.ArgumentParser(description=name)
 
-    parser.add_argument("-v", "--verbose", action="store_true", help="Show output from latex and dvipng commands", default=False)
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="store_true",
+        help="Show output from latex and dvipng commands",
+        default=False,
+    )
     parser.add_argument("leftFn", type=str, help="Filename of left table")
-    parser.add_argument("leftPK", type=str, help="Name of column to use as the primary key for the left table")
-    parser.add_argument("rightFn", type=str, help="Filename of the right table")
-    parser.add_argument("rightPK", type=str, help="Name of column to use as the primary key for the right table")
+    parser.add_argument(
+        "leftPK",
+        type=str,
+        help="Name of column to use as the primary key for the left table",
+    )
+    parser.add_argument(
+        "rightFn", type=str, help="Filename of the right table"
+    )
+    parser.add_argument(
+        "rightPK",
+        type=str,
+        help="Name of column to use as the primary key for the right table",
+    )
     parser.add_argument("outputFn", type=str, help="Output filename")
-    parser.add_argument("-t", "--type",  type=str, action="store", dest="joinType", choices=["left","right","inner","full"], help="Type of join (default is 'left join')", default="left")
+    parser.add_argument(
+        "-t",
+        "--type",
+        type=str,
+        action="store",
+        dest="joinType",
+        choices=["left", "right", "inner", "full"],
+        help="Type of join (default is 'left join')",
+        default="left",
+    )
 
     return parser.parse_args(argList)
+
 
 def main():
 
     args = doArgs(sys.argv[1:], "CSV join script")
     startTime = float(time.time())
 
-    #-------------------------------------------------------------------------------------------
+    # -------------------------------------------------------------------------------------------
     # Load the left data file
-    #-------------------------------------------------------------------------------------------
+    # -------------------------------------------------------------------------------------------
     leftFn = args.leftFn
     leftPK = args.leftPK
 
@@ -78,12 +111,12 @@ def main():
 
     # Map the primary keys to their row index so we can look up keys from the other table in constant time
     leftPKMap = {}
-    for i,row in enumerate(leftData):
+    for i, row in enumerate(leftData):
         leftPKMap[row[leftKeyIndex]] = i
 
-    #-------------------------------------------------------------------------------------------
+    # -------------------------------------------------------------------------------------------
     # Load the right data file
-    #-------------------------------------------------------------------------------------------
+    # -------------------------------------------------------------------------------------------
     rightFn = args.rightFn
     rightPK = args.rightPK
 
@@ -91,20 +124,22 @@ def main():
 
     # Map the primary keys to their row index so we can look up keys from the other table in constant time
     rightPKMap = {}
-    for i,row in enumerate(rightData):
+    for i, row in enumerate(rightData):
         rightPKMap[row[rightKeyIndex]] = i
 
-    #-------------------------------------------------------------------------------------------
+    # -------------------------------------------------------------------------------------------
     # Write output file
-    #-------------------------------------------------------------------------------------------
+    # -------------------------------------------------------------------------------------------
     outputFn = args.outputFn
     joinType = args.joinType
 
-    f = open(outputFn, 'w')
-    csvWriter = csv.writer(f, delimiter=',',quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    f = open(outputFn, "w")
+    csvWriter = csv.writer(
+        f, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL
+    )
     csvWriter.writerow(leftHeader + rightHeader)
 
-    #-------------------------------------------------------------------------------------------
+    # -------------------------------------------------------------------------------------------
     if joinType == "left":
 
         # Iterate through each row in the left table, try to find the matching row in the right table
@@ -118,7 +153,7 @@ def main():
                 rightRow = ["null"] * len(rightHeader)
 
             csvWriter.writerow(leftRow + rightRow)
-    #-------------------------------------------------------------------------------------------
+    # -------------------------------------------------------------------------------------------
     elif joinType == "right":
 
         # Similar to 'left' case
@@ -131,7 +166,7 @@ def main():
                 leftRow = ["null"] * len(leftHeader)
 
             csvWriter.writerow(leftRow + rightRow)
-    #-------------------------------------------------------------------------------------------
+    # -------------------------------------------------------------------------------------------
     elif joinType == "inner":
         # This join will only write rows for primary keys in the intersection of the two primary key sets
         leftKeySet = set(leftPKMap.keys())
@@ -144,7 +179,7 @@ def main():
             rightRow = rightData[rightPKMap[keyVal]]
 
             csvWriter.writerow(leftRow + rightRow)
-    #-------------------------------------------------------------------------------------------
+    # -------------------------------------------------------------------------------------------
     elif joinType == "full":
         # This join will only write rows for primary keys in the union of the two primary key sets
         leftKeySet = set(leftPKMap.keys())
@@ -158,7 +193,7 @@ def main():
                 leftRow = leftData[leftPKMap[keyVal]]
             else:
                 leftRow = ["null"] * len(leftHeader)
-            
+
             rightRow = None
             if keyVal in rightPKMap:
                 rightRow = rightData[rightPKMap[keyVal]]
@@ -166,7 +201,7 @@ def main():
                 rightRow = ["null"] * len(rightHeader)
 
             csvWriter.writerow(leftRow + rightRow)
-    #-------------------------------------------------------------------------------------------
+    # -------------------------------------------------------------------------------------------
     else:
         raise Exception("This shouldn't happen")
 
